9d1ac8cba7af876ae9196aa2a0bb0b3492949fe8
sylvainloiseau <sylvain.loiseau@univ-paris13.fr>
2024-10-03 06:41:42 +0200

    new

---
+++ tests/test_rico_namespace.py
@@ -0,0 +1,35 @@
+from rico_namespace import RICO, RicoOwl
+from owlready2 import ThingClass
+
+class TestRicoNamespace:
+    def test_rico(request):
+        pass
+
+class TestRicoOwl:
+
+    @staticmethod
+    def test_rico_get_classes(capsys, request):
+        rico = RicoOwl()
+        classes = rico.get_classes()
+        with capsys.disabled():
+            l = 0
+            for c in classes:
+                print(f"class: {c}")
+                print(type(c))
+                print(str(c))
+                l+=1
+            print(f"{l} classes found")
+            
+
+    @staticmethod
+    def test_rico_get_info(capsys, request):
+        rico = RicoOwl()
+        classes = rico.get_classes()
+        with capsys.disabled():
+            for c in classes:
+                print(f"-----------------------")
+                print(f"class: {c}")
+                for i in ThingClass.instances(c):
+                    print(f"\tinstance: {i}")
+                for p in ThingClass.get_class_properties(c):
+                    print(f"\tproperty: {p}")
